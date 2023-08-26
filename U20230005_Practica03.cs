1d46c115003dce819fbf8d36962bf3c1e2c1a14f
Msalmeron95 <martinsstudent@gmail.com>
2023-08-26 17:20:19 -0400

    Practica3

--- U20230005_Practica03.cs
+++ U20230005_Practica03.cs
@@ -6,7 +6,29 @@ namespace MyApp// Note: actual namespace depends on the project name.
     {
          static void Main(string[] args)
         {
-            
+            //Declaracion de variables.
+            string? Marca_Zapato;
+            string? Color_Zapato;
+            int cantidad;
+            double precio;
+
+            //Capturando datos.
+            Console.WriteLine("\n------------------- Capturando Variables -------------------\n");
+            Console.WriteLine("Ingrese la marca del Zapato a comprar:\n ");
+            Marca_Zapato = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el color del Zapato a comprar:\n ");
+            Color_Zapato = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad de Zapatos a comprar:\n ");
+            cantidad = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el precio del par Zapato:\n ");
+            precio = Convert.ToDouble(Console.ReadLine());
+
+            //
+
+
         }
     }
 }
