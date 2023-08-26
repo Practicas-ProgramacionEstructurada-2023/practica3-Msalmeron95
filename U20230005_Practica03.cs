fcf3c6ab55ff4eea2f7cf16784e27a72297bce2f
Msalmeron95 <martinsstudent@gmail.com>
2023-08-26 17:38:19 -0400

    PRACTICA3

--- U20230005_Practica03.cs
+++ U20230005_Practica03.cs
@@ -14,21 +14,30 @@ namespace MyApp// Note: actual namespace depends on the project name.
 
             //Capturando datos.
             Console.WriteLine("\n------------------- Capturando Variables -------------------\n");
-            Console.WriteLine("Ingrese la marca del Zapato a comprar:\n ");
+            Console.WriteLine("\nIngrese la marca del Zapato a comprar: ");
             Marca_Zapato = Convert.ToString(Console.ReadLine());
 
-            Console.WriteLine("Ingrese el color del Zapato a comprar:\n ");
+            Console.WriteLine("\nIngrese el color del Zapato a comprar: ");
             Color_Zapato = Convert.ToString(Console.ReadLine());
 
-            Console.WriteLine("Ingrese la cantidad de Zapatos a comprar:\n ");
+            Console.WriteLine("\nIngrese la cantidad de Zapatos a comprar: ");
             cantidad = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine("Ingrese el precio del par Zapato:\n ");
+            Console.WriteLine("\nIngrese el precio del par Zapato: ");
             precio = Convert.ToDouble(Console.ReadLine());
 
-            //
+            //Capturando el total a pagar.
+            double Total = cantidad * precio;
 
 
+        //Imprimiendo resultado
+        Console.WriteLine("\n------------------- Imprimiendo resultados -------------------\n");
+        Console.WriteLine("\n La marca de zapatos a comprar es: " + Marca_Zapato);
+        Console.WriteLine("\n El color de los zapatos a comprar es: " + Color_Zapato);
+        Console.WriteLine("\n La cantidad de zapatos a comprar es: " + cantidad);
+        Console.WriteLine("\n El precio poar de zapatos a comprar es: " + precio);
+        Console.WriteLine("\n El total a pagar es: " + "$" + Total);
+        Console.WriteLine("\n------------------- Fin imprimiendo resultados -------------------\n");
         }
     }
 }
